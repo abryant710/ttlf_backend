9af86f80df443906afa8f8ed9382e18bd6f39e8f
Alex Bryant <alexbryant710@gmail.com>
2020-12-23 19:46:27 +0000

    Adding database and test for the connection

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+/* eslint-disable no-console */
 // Native node packages
 
 // 3rd party packages
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Pool } = require('pg');
 
 const app = express();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 const adminRoutes = require('./routes/admin');
 const apiRoutes = require('./routes/api');
@@ -15,6 +24,19 @@ app.use(bodyParser.urlencoded({
 
 app.use(adminRoutes);
 app.use('/api', apiRoutes);
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM users');
+    const results = { results: (result) ? result.rows : null };
+    res.render('pages/db', results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send(`Error ${err}`);
+  }
+});
 
 app.use('/', (_req, res) => {
   res.status(404).send('<h1>This page doesn\'t exist</h1>');
