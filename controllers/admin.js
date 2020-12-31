e9611aac2da1e803760a4f350844eb350691be14
Alex Bryant <alexbryant710@gmail.com>
2020-12-31 01:50:21 +0000

    Ran linter

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,6 @@
 const { pages: { CONFIG_HOME_PAGE } } = require('../utils/pages');
 
-module.exports.getConfig = (req, res) => res
+module.exports.getConfig = (_req, res) => res
   .status(200)
   .render(CONFIG_HOME_PAGE, {
     configPage: 'live',
