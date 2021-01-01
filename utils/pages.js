21ced21ea53e5820dc9c293f48d155548a924fb8
Alex Bryant <alexbryant710@gmail.com>
2021-01-01 18:02:07 +0000

    Adding extra pages to utils file. Making nav header light version

--- utils/pages.js
+++ utils/pages.js
@@ -2,4 +2,6 @@ module.exports.pages = {
   CREATE_ADMIN_PAGE: 'pages/loggedIn/create-admin',
   CONFIG_HOME_PAGE: 'pages/loggedIn/config',
   LOGIN_PAGE: 'pages/loggedOut/login',
+  SEND_RESET_PAGE: 'pages/loggedOut/send-reset',
+  RESET_PW_PAGE: 'pages/loggedOut/reset-password',
 };
