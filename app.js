24d41cfeb18ef59fd8586f853736a4980f37eb42
Alex Bryant <alexbryant710@gmail.com>
2021-01-16 19:15:12 +0000

    Adding lots of comments to app.js

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const multer = require('multer');
 // https://www.npmjs.com/package/validatorjs
 
 const DEFAULT_ROUTE = '/config/live';
-
 const {
   TTLF_MONGO_USER,
   TTLF_MONGO_PW,
@@ -33,8 +32,8 @@ const store = new MongoDBStore({
 });
 const csrfProtection = csrf();
 
-app.set('view engine', 'ejs');
-app.set('views', 'src/views');
+app.set('view engine', 'ejs'); // set template engine to ejs
+app.set('views', 'src/views'); // use src/views folders to store view files
 
 const { get404, get500 } = require('./src/controllers/error');
 const authRoutes = require('./src/routes/auth');
@@ -42,7 +41,7 @@ const superAdminRoutes = require('./src/routes/superAdmin');
 const adminRoutes = require('./src/routes/admin');
 const apiRoutes = require('./src/routes/api');
 
-const fileStorage = multer.diskStorage({
+const fileStorage = multer.diskStorage({ // config file storage for uploads
   destination: (req, file, cb) => {
     cb(null, 'public/images/flyer');
   },
@@ -52,7 +51,7 @@ const fileStorage = multer.diskStorage({
   },
 });
 
-const fileFilter = (req, file, cb) => {
+const fileFilter = (req, file, cb) => { // set file mime types
   const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
   if (validMimeTypes.includes(file.mimetype)) {
     cb(null, true);
@@ -63,8 +62,8 @@ const fileFilter = (req, file, cb) => {
 app.use(bodyParser.json()); // json api data
 app.use(bodyParser.urlencoded({ extended: false })); // form data
 app.use(multer({ storage: fileStorage, fileFilter }).single('image')); // image uploads
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
+app.use(express.static(path.join(__dirname, 'public'))); // make static folder public
+app.use(session({ // configure auth cookies
   secret: TTLF_SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
@@ -75,8 +74,7 @@ app.use(session({
 }));
 app.use(csrfProtection); // protect form submission from csrf
 app.use(flash()); // allow messages to be flashed
-
-app.use((req, res, next) => {
+app.use((req, res, next) => { // set local variables for use in templates
   const { isSuperAdmin, email: userEmail } = req.session.user || {};
   res.locals.csrfToken = req.csrfToken();
   res.locals.isSuperAdmin = isSuperAdmin;
@@ -84,15 +82,16 @@ app.use((req, res, next) => {
   next();
 });
 
+// define the various routes
 app.use(authRoutes);
 app.use(superAdminRoutes);
 app.use(adminRoutes);
 app.use('/api', apiRoutes);
-
+// redirect to default route for /
 app.get(/^\/$/, (_req, res) => res
   .status(200)
   .redirect(DEFAULT_ROUTE));
-
+// set routes for 500 error page and 404 not found page
 app.get('/500', get500);
 app.use(get404);
 
@@ -102,25 +101,29 @@ app.use((error, req, res, next) => {
   return res.redirect('/500');
 });
 
+// create folder for flyer files if it doesn't already exist
 const eventDir = path.join(__dirname, 'public/images/flyer');
 if (!fs.existsSync(eventDir)) {
   fs.mkdirSync(eventDir);
 }
 
-mongoose.connect(
-  `${MONGO_DB_URI}?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  },
-)
-  .then(() => {
+// spin up the application by connecting to the database and listening on port 5000
+const startApp = async () => {
+  try {
+    await mongoose.connect(
+      `${MONGO_DB_URI}?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      },
+    );
     console.info(`Successfully connected to the ${TTLF_MONGO_DB} database`);
+    console.info(`Running the ${TTLF_ENV} environment`);
     app.listen(process.env.PORT || 5000);
-  })
-  .catch((err) => {
+  } catch (err) {
     console.error(err);
-  });
+  }
+};
 
-console.info(`Running the ${TTLF_ENV} environment`);
+startApp();
