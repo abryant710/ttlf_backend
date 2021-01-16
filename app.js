15e62ebbb3dd8174504ec26bf6f09b0fbb537128
Alex Bryant <alexbryant710@gmail.com>
2021-01-16 17:20:38 +0000

    Updating allowed origins

--- app.js
+++ app.js
@@ -23,7 +23,14 @@ const {
   TTLF_ENV,
 } = process.env;
 
-const API_ALLOWED_ORIGINS = TTLF_ENV === 'production' ? 'https://*.ttlf.net' : '*';
+const allowedOrigins = [];
+if (TTLF_ENV === 'production') {
+  allowedOrigins.push('https://www.ttlf.net');
+  allowedOrigins.push('https://ttlf.net');
+  allowedOrigins.push('https://admin.ttlf.net');
+} else {
+  allowedOrigins.push('*');
+}
 
 const MONGO_DB_URI = `mongodb+srv://${TTLF_MONGO_USER}:${TTLF_MONGO_PW}@${TTLF_MONGO_URI}/${TTLF_MONGO_DB}`;
 
@@ -77,7 +84,9 @@ app.use(session({
 app.use(csrfProtection); // protect form submission from csrf
 app.use(flash()); // allow messages to be flashed
 app.use((_req, res, next) => { // add headers to allow REST api to recieve requests
-  res.setHeader('Access-Control-Allow-Origin', API_ALLOWED_ORIGINS);
+  allowedOrigins.forEach((origin) => {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  });
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
